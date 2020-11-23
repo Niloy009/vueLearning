c8298d92a9a9facdba5a33f0be70cfa25ca6346a
Niloy009 <niloy.roy@northsouth.edu>
2020-11-24 01:02:55 +0600

    2 way data binding, computed properties, dynamic css class, condition, loop, template

--- app.js
+++ app.js
@@ -7,7 +7,21 @@ new Vue({
         websiteTag: '<a href="http://www.facebook.com">Facebook</a>',
         age: 25,
         x: 0,
-        y: 0
+        y: 0,
+        nam: '',
+        year: '',
+        a: 0,
+        b: 0,
+        available: false,
+        nearby: false,
+        clkA: false,
+        clkB: false,
+        skills: ['java', 'C', 'C++', 'PHP', 'Javascript'],
+        managers: [
+            {name:'niloy', age:26},
+            {name:'rajon', age:27},
+            {name:'john', age:28}
+        ]
     },
     methods: {
         greet: function (dayTime) {
@@ -19,9 +33,39 @@ new Vue({
         sub: function (dec) {
             this.age -= dec;
         },
-        updateXY: function (event){
+        updateXY: function (event) {
             this.x = event.offsetX;
             this.y = event.offsetY;
+        },
+        logName: function () {
+            console.log('keypressed for name field');
+        },
+        logYear: function () {
+            console.log('keypressed for year field');
+        },
+        // addToA: function (){
+        //     console.log('addToA');
+        //     return this.a + this.age;
+        // },
+        // addToB: function (){
+        //     console.log('addToB');
+        //     return this.b + this.age;
+        // }
+    },
+    computed: {
+        addToA: function () {
+            console.log('addToA');
+            return this.a + this.age;
+        },
+        addToB: function () {
+            console.log('addToB');
+            return this.b + this.age;
+        },
+        computedClass: function () {
+            return {
+                available: this.available,
+                nearby: this.nearby
+            }
         }
     }
 });
